ec683671330e08c42a8b37f0c0e59ca90fd5c967
Manuel del Castillo <zurdodel83@gmail.com>
2023-04-09 19:41:22 -0300

    conexion co nMongoDB Atlas

--- db/client.py
+++ db/client.py
@@ -1,3 +1,7 @@
 from pymongo import MongoClient
 
-db_client = MongoClient()
+# conexion a la DDBB local
+#db_client = MongoClient().local
+
+#conexion a la DDBB remota
+db_client = MongoClient("mongodb+srv://zurdodel83:mq5i5979FWbkfep8@fastapi-1ddbb.nfgdfz9.mongodb.net/?retryWrites=true&w=majority").test
--- routers/users_db.py
+++ routers/users_db.py
@@ -13,14 +13,14 @@ router = APIRouter(
 
 def search_user(field: str, key): 
     try:
-        user = db_client.local.users.find_one({field: key})
+        user = db_client.users.find_one({field: key})
         return User(**user_schema(user))
     except:
         return {"error": "No se ha encontrado el usuario"}
 
 @router.get("/", response_model=list[User])
 async def users():
-    return users_schema(db_client.local.users.find())
+    return users_schema(db_client.users.find())
 
 @router.get("/{id}")
 async def user(id: str):
@@ -36,9 +36,9 @@ async def user(user: User):
     user_dict = dict(user)
     # eliminamos es campo id del dict ya que MongoDB asigna un id unico al generar el registro
     del user_dict["id"]
-    id = db_client.local.users.insert_one(user_dict).inserted_id
+    id = db_client.users.insert_one(user_dict).inserted_id
     
-    new_user = user_schema(db_client.local.users.find_one({"_id": id}))
+    new_user = user_schema(db_client.users.find_one({"_id": id}))
     # utilizando el esquema de la funcion schema pasamos el user insertado
     # en la DDBB al formato de usuario que utilizamos en la API
 
@@ -51,7 +51,7 @@ async def user(user: User):
     del user_dict["id"]
 
     try:
-        db_client.local.users.find_one_and_replace({"_id": ObjectId(user.id)}, user_dict)
+        db_client.users.find_one_and_replace({"_id": ObjectId(user.id)}, user_dict)
     except:
         return {"error": "No se ha encontrado el usuario"}
 
@@ -59,7 +59,7 @@ async def user(user: User):
     
 @router.delete("/{id}")
 async def user(id: str):
-    found = db_client.local.users.find_one_and_delete({"_id": ObjectId(id)})
+    found = db_client.users.find_one_and_delete({"_id": ObjectId(id)})
     
     if not found:
         return {"error": "nose ha encontrado el usuario"}
